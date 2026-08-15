b3996f8c96958ea2f2cb6df38ff985bbdb13e9ea
HZ <tenbigforms@gmail.com>
2026-08-15 19:46:12 +0200

    Update chapter 12

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,42 +1,58 @@
 import type { Course } from "@/types/course";
-import { useState } from "react";
-import { Button, Image, ScrollView, StyleSheet, Text, View } from "react-native";
+import { useEffect, useState } from "react";
+import { Button, ScrollView, StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function Index() {
 
-  const [courses, setCourses] = useState<Course[]>([]);
 
-  const fetchCourses = async () => {
+  const [count, setCount] = useState(0)
+  const [courses, setCourses] = useState<Course[]>([])
+  const [keyword, setKeyword] = useState('')
+  const [loading, setLaoding] = useState(true)
+
+
+  const fetchData = async () => {
     if (courses.length > 0) {
       setCourses([]);
-      return;
+      return
     } else {
       try {
-        const res = await fetch("http://v1.kisbook.com:3000/search");
-        const { data } = await res.json();
-        setCourses(data.courses);
+        const res = await fetch(`http://v1.kisbook.com:3000/search?q=${keyword}`)
+        const { data } = await res.json()
+        setCourses(data.courses)
       } catch (error) {
-        console.error("Error fetching courses:", error);
+        console.error("Error fetching courses:", error)
       }
     }
   };
-
+  useEffect(() => {
+    fetchData();
+  }, [keyword]);
   return (
     <View style={styles.container}>
-      <Button title="Fetch Courses" onPress={fetchCourses} />
-
+      <Text>{count}times</Text>
+      <Button
+        title="Click me"
+        onPress={() => setCount(count + 1)}
+      />
+      <Text>U R SEARHING FOR:{keyword}</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Please type STH"
+        onChangeText={text => setKeyword(text)}
+        defaultValue={keyword}
+      />
       <ScrollView>
         {
           courses.map((course) => (
             <View key={course.id}>
-              <Image source={{uri: course.user.avatar}} style={styles.avatar} />
               <Text>{course.name}</Text>
             </View>
           ))
         }
       </ScrollView>
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
@@ -46,10 +62,14 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
   },
-
-  avatar: {
-    width: 50,
-    height: 50,
-    borderRadius: 10,
-  }
+  input: {
+    height: 40,
+    width: 300,
+    margin: 12,
+    padding: 10,
+    borderWidth: 1,
+    borderColor: '#000000',
+    borderRadius: 5,
+    color: '#111192',
+  },
 });
