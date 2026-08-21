45cc37497e18e305c7eb34187740e667870927f3
kevin Zhang <lucyjones2059@gmail.com>
2026-08-21 18:43:59 +0200

    Update

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -4,23 +4,21 @@ export default function Layout() {
   return (
     <Stack
       screenOptions={{
-        title: '',                      // 默认标题为空
-        headerTitleAlign: 'center',     // 安卓标题栏居中
-        animation: 'simple_push',       // 安卓使用左右切屏
-        headerTintColor: '#1f99b0',     // 导航栏中文字、按钮、图标的颜色
-        // 标题组件的样式
+        title: '',
+        headerTitleAlign: 'center',
+        animation: 'simple_push',
+        headerTintColor: '#1f99b0',
+
         headerTitleStyle: {
           fontWeight: '400',
           color: '#2A2929',
           fontSize: 16,
         },
-        headerBackButtonDisplayMode: 'minimal', // 设置返回按钮只显示箭头，不显示文字
+        headerBackButtonDisplayMode: 'minimal',
       }}
     >
-      {/* Tabs */}
       <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
 
-      {/* Cards */}
       <Stack.Screen name="articles/index" options={{ title: '通知' }} />
       <Stack.Screen name="settings/index" options={{ title: '设置' }} />
       <Stack.Screen name="courses/[id]" options={{ title: '课程详情' }} />
