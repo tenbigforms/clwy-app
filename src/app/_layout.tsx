c352f0dd89bde1b0b8a74fb95d733357d6044ab9
kevin Zhang <lucyjones2059@gmail.com>
2026-08-21 18:05:30 +0200

    Add a simplest <Tabs/>

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,4 +1,4 @@
-import { Stack, Link } from 'expo-router';
+import { Tabs, Stack, Link } from 'expo-router';
 import { Image } from 'expo-image';
 import { SimpleLineIcons } from '@expo/vector-icons';
 import { StyleSheet, TouchableOpacity } from 'react-native';
@@ -21,40 +21,41 @@ function HeaderButton(props) {
 }
 export default function Layout() {
   return (
-    <Stack
-      screenOptions={{
-        title: '',
-        headerTitleAlign: 'center',
-        animation: 'simple_push',
-        headerTintColor: '#1f99b0',
-        headerTitleStyle: {
-          fontWeight: '400',
-          color: '#2A2929',
-          fontSize: 16,
-        },
-        headerBackButtonDisplayMode: 'minimal',
-      }}
-    >
-      <Stack.Screen
-        name="index"
-        options={{
-          headerTitle: props => <LogoTitle {...props} />,
-          headerLeft: () => <HeaderButton name="bell" href="/articles" style={styles.headerButton} />,
-          headerRight: () => (
-            <>
-              <HeaderButton name="magnifier" href="/search" style={[styles.headerButton, styles.searchButton]} />
-              <HeaderButton name="options" href="/settings" style={styles.headerRight} />
-            </>
-          ),
-        }}
-      />
-      <Stack.Screen
-        name="courses/[id]"
-        options={({ route }) => ({
-          title: route.params?.title || 'Course Page',
-        })}
-      />
-    </Stack>
+    <Tabs />
+    // <Stack
+    //   screenOptions={{
+    //     title: '',
+    //     headerTitleAlign: 'center',
+    //     animation: 'simple_push',
+    //     headerTintColor: '#1f99b0',
+    //     headerTitleStyle: {
+    //       fontWeight: '400',
+    //       color: '#2A2929',
+    //       fontSize: 16,
+    //     },
+    //     headerBackButtonDisplayMode: 'minimal',
+    //   }}
+    // >
+    //   <Stack.Screen
+    //     name="index"
+    //     options={{
+    //       headerTitle: props => <LogoTitle {...props} />,
+    //       headerLeft: () => <HeaderButton name="bell" href="/articles" style={styles.headerButton} />,
+    //       headerRight: () => (
+    //         <>
+    //           <HeaderButton name="magnifier" href="/search" style={[styles.headerButton, styles.searchButton]} />
+    //           <HeaderButton name="options" href="/settings" style={styles.headerRight} />
+    //         </>
+    //       ),
+    //     }}
+    //   />
+    //   <Stack.Screen
+    //     name="courses/[id]"
+    //     options={({ route }) => ({
+    //       title: route.params?.title || 'Course Page',
+    //     })}
+    //   />
+    // </Stack>
   )
 }
 
