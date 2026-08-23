ff5773845c11994b59123b999799c1cc11acf34f
kevin Zhang <lucyjones2059@gmail.com>
2026-08-23 12:07:42 +0200

    Update

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -36,16 +36,6 @@ export default function Index() {
 
     <ScrollView style={styles.container}>
 
-
-      <Text style={styles.title}>Home Page</Text>
-
-      <Link style={styles.link} href="/courses/1?title=Node.js">
-        Check Node.js Course
-      </Link>
-
-      <Link style={styles.link} href="/teachers/1">
-        Modal
-      </Link>
     </ScrollView>
   )
 }
@@ -54,47 +44,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  input: {
-    height: 40,
-    width: 300,
-    margin: 12,
-    padding: 10,
-    borderWidth: 1,
-    borderColor: '#000000',
-    borderRadius: 5,
-    color: '#111192',
-  },
-  header: {
-    textAlign: "center",
-    fontSize: 40,
-    lineHeight: 60,
-    fontWeight: "bold",
-    marginVertical: 10,
-  },
-  footer: {
-    textAlign: "center",
-    fontSize: 18,
-    lineHeight: 40,
-    color: "#999",
-  },
-
-  buttonText: {
-    marginTop: 20,
-    fontSize: 25,
-    color: '#ff7f6f',
-  },
-  title: {
-    fontSize: 40,
-    fontWeight: 'bold',
-    color: '#e29447',
-  },
-  link: {
-    marginTop: 20,
-    fontSize: 20,
-    color: '#1f99b0',
   },
 })
 
