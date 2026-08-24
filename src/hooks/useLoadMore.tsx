57786368012ea230ad3b96cbc9101da545ad3b2c
kevin Zhang <lucyjones2059@gmail.com>
2026-08-24 11:51:58 +0200

    Edit useLoadMore.tsx

--- src/hooks/useLoadMore.tsx
+++ src/hooks/useLoadMore.tsx
@@ -6,17 +6,28 @@ import { get } from '@/utils/request'
 
 export default function useLoadMore(url, key, setData) {
     const [page, setPage] = useState(1)
+    const [loading, setLoading] = useState(false)
+    const [hasMore, setHasMore] = useState(true)
 
     const onEndReached = async () => {
+        if (loading) return
+        if (!hasMore) return
 
+        setLoading(true)
         const nextPage = page + 1
         setPage(nextPage)
-
-        const { data } = await get(url, { page: nextPage })
-
-        setData((prevData) => ({
-            articles: [...prevData.articles, ...data.articles],
-        }))
+        try {
+            const { data } = await get(url, { page: nextPage })
+            if (data[key].length === 0) {
+                setHasMore(false)
+            } else {
+                setData((prevData) => ({
+                    [key]: [...prevData[key], ...data[key]],
+                }))
+            }
+        } finally {
+            setLoading(false)
+        }
     }
 
     return { onEndReached }
