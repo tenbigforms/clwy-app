7e9121f9f1954e3e6cf0fe8990c5a2b00b519d0f
kevin Zhang <lucyjones2059@gmail.com>
2026-08-25 11:04:05 +0200

    npx expo install react-native-pager-view clwy-expo -scrollable-tab-view
    add simple thing in video page
    declare:
    declare module 'clwy-expo-scrollable-tab-view'
    declare module 'clwy-react-native-tableview-simple'

--- env.d.ts
+++ env.d.ts
@@ -1,1 +1,3 @@
-declare module 'urlcat';
+declare module 'urlcat'
+declare module 'clwy-expo-scrollable-tab-view'
+declare module 'clwy-react-native-tableview-simple'
--- src/app/(tabs)/videos.tsx
+++ src/app/(tabs)/videos.tsx
@@ -1,15 +1,27 @@
-import { View, Text, StyleSheet } from 'react-native'
-import { Link } from 'expo-router'
+import { StyleSheet, Text } from 'react-native'
+import ScrollableTabView, { ScrollableTabBar } from 'clwy-expo-scrollable-tab-view'
 
 export default function Index() {
     return (
-        <View style={styles.container}>
-            <Text style={styles.title}>Video Page</Text>
-
-            <Link style={styles.link} href="/courses/2?title=React Native">
-                Check React Native Course
-            </Link>
-        </View>
+        <ScrollableTabView
+            style={styles.container}
+            initialPage={0}
+            renderTabBar={() => <ScrollableTabBar />}
+            tabBarUnderlineStyle={styles.barUnderLine}
+            tabBarBackgroundColor={'#fff'}
+            tabBarInactiveTextColor={'#777'}
+            tabBarActiveTextColor={'#000'}
+            tabBarTextStyle={styles.barText}
+        >
+            <Text tabLabel="新闻">新闻的内容</Text>
+            <Text tabLabel="体育">体育的内容</Text>
+            <Text tabLabel="生活">生活的内容</Text>
+            <Text tabLabel="娱乐">娱乐的内容</Text>
+            <Text tabLabel="科技">科技的内容</Text>
+            <Text tabLabel="创业">创业的内容</Text>
+            <Text tabLabel="教育">教育内容</Text>
+            <Text tabLabel="财经">财经的内容</Text>
+        </ScrollableTabView>
     )
 }
 
@@ -17,17 +29,12 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
     },
-    title: {
-        fontSize: 40,
-        fontWeight: 'bold',
-        color: '#e29447',
+    barUnderLine: {
+        backgroundColor: '#1f99b0',
+        height: 2,
     },
-    link: {
-        marginTop: 20,
-        fontSize: 20,
-        color: '#1f99b0',
+    barText: {
+        fontWeight: '400',
     },
 })
